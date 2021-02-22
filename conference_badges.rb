9ccda77cb3103b42381041fd6b52c67444213ef6
Christopher Kim <chris.kim1990@gmail.com>
2021-02-22 18:48:20 -0500

    commit tree a1c7ecc2f80a425f151d20fa09355ab2cd401b03

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,32 @@
-# Write your code here.
+require 'pry'
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name)
+    list = []
+    name.map do |name|
+        list << "Hello, my name is #{name}."
+    end
+    list
+end
+
+def assign_rooms(array)
+    room = 0
+     array.map do |name|
+        room += 1
+        "Hello, #{name}! You'll be assigned to room #{room}!"
+     end
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |value|
+        puts value
+    end
+    assign_rooms(attendees).each do |a_value|
+        puts a_value
+    end
+end
+        
+
+
